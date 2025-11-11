b46940218246536dea29a02786c31045b6927759
bouncewater <bouncewater48@gmail.com>
2025-11-11 16:42:57 +0900

    [강승훈] - database/UsedGoodsController.java 수정 - database/service/UsedGoodsService/java 수정 - mappers/usedGoodsMapper.xml 수정

--- src/main/java/com/bounce/springexample/database/service/UsedGoodsService.java
+++ src/main/java/com/bounce/springexample/database/service/UsedGoodsService.java
@@ -1,17 +1,27 @@
 package com.bounce.springexample.database.service;
 
+import com.bounce.springexample.database.domain.UsedGoods;
+import com.bounce.springexample.database.repasitory.UsedGoodsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 // 부가적인 기능 수행
-//데이터 가공 처리
+// 데이터 가공 처리
 @Service
 public class UsedGoodsService {
 
+    // 의존성 주입 - DI
+    // Spring Framework가 직접 객체 생성 관리
+    @Autowired
+    private UsedGoodsRepository usedGoodsRepository;
+
     // 중고 거래 게시글 리스트 얻어오기
-    public getUsedGoodsList() {
+    public List<UsedGoods> getUsedGoodsList() {
 
-
-
+        // used_goods 모든 행 조회 결과
+        List<UsedGoods> usedGoodsList = usedGoodsRepository.selectUsedGoodsList();
+        return usedGoodsList;
     }
-
 }
