dfabc757a53cf5cd97553a70b892e069ffb8c31d
bouncewater <bouncewater48@gmail.com>
2025-11-14 17:48:51 +0900

    [강승훈] - mvc/user 예제 완성

--- src/main/java/com/bounce/springexample/mvc/UserController.java
+++ src/main/java/com/bounce/springexample/mvc/UserController.java
@@ -37,9 +37,18 @@ public class UserController {
             ,@RequestParam("email") String email
             ,@RequestParam(value="introduce", required = true) String introduce) {
 
-        int count = userService.createUser(name, birthday, email, introduce);
+//        int count = userService.createUser(name, birthday, email, introduce);
 
-        return "입력 결과 : " + count;
+        User user = new User();
+
+        user.setName(name);
+        user.setYyyymmdd(birthday);
+        user.setEmail(email);
+        user.setIntroduce(introduce);
+
+        int count = userService.createUserByObject(user);
+
+        return "입력 결과 : " + count + "저장된 index : " + user.getId();
     }
 
     @GetMapping("/form")
--- src/main/java/com/bounce/springexample/mvc/service/UserService.java
+++ src/main/java/com/bounce/springexample/mvc/service/UserService.java
@@ -32,4 +32,9 @@ public class UserService {
         return count;
     }
 
+    public int createUserByObject(User user) {
+        int count = userRepository.insertUserByObject(user);
+        return count;
+    }
+
 }
